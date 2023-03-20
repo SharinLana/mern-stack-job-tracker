b17053ab6114942700e55414a5f9826b660f6f11
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-03-20 15:20:22 -0700

    create token verification middleware - auth.js

---
+++ middleware/auth.js
@@ -0,0 +1,21 @@
+import jwt from "jsonwebtoken";
+import { UnauthorizedError } from "../errors/index.js";
+
+const auth = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    throw new UnauthorizedError("Authentication invalid!");
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = { userId: payload.userId, name: payload.name };
+    next();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export default auth;
