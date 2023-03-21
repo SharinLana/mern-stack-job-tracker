9076c13ac35e8d53f2b9ebf36d2117b7e9962b56
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-03-21 12:22:53 -0700

    create basic getAllJobs controller in job-controllers.js

--- controllers/job-controllers.js
+++ controllers/job-controllers.js
@@ -1,6 +1,15 @@
+import { StatusCodes } from "http-status-codes";
+import JobModel from "../models/jobModel.js";
+
 const getJobs = async (req, res, next) => {
-    res.send("All Jobs");
-  };
+  const jobs = await JobModel.find({ createdBy: req.user.userId });
+
+  res.send(StatusCodes.OK).json({
+    status: "success",
+    totalJobs: jobs.length,
+    jobs,
+  });
+};
 
 const addJob = async (req, res, next) => {
   res.send("Add job");
