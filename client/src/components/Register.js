0d59c3c93bddd6921565be9e6f74fd10265f28c2
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-03-22 13:02:40 -0700

    import registerUser function to Register.js and invoke it inside of the formSubmitHandler

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Wrapper from "../assets/wrappers/Register";
+import { useAppContext } from "../context/appContext";
 import Logo from "./Logo";
 import FormInput from "./sharedComponents/FormInput";
 import FormLabel from "./sharedComponents/FormLabel";
@@ -7,7 +8,7 @@ import FormLabel from "./sharedComponents/FormLabel";
 const initialState = {
   firstName: "",
   lastName: "",
-  location: "",
+  userLocation: "",
   email: "",
   password: "",
   isMember: true,
@@ -15,6 +16,7 @@ const initialState = {
 
 const Register = () => {
   const [inputValue, setInputValue] = useState(initialState);
+  const { registerUser } = useAppContext();
 
   const getInputValueHandler = (e) => {
     setInputValue((currentValues) => {
@@ -24,6 +26,16 @@ const Register = () => {
 
   const formSubmitHandler = (e) => {
     e.preventDefault();
+
+    const { firstName, lastName, userLocation, email, password, isMember } =
+      inputValue;
+
+    const currentUser = { firstName, lastName, userLocation, email, password };
+    if (isMember) {
+      console.log("Already a member");
+    } else {
+      registerUser(currentUser);
+    }
   };
 
   const toggleBtnHandler = () => {
@@ -33,112 +45,110 @@ const Register = () => {
   };
 
   return (
+    <Wrapper>
+      <main className="form-container">
+        <form onSubmit={formSubmitHandler}>
+          <div className="logo-container">
+            <Logo />
+          </div>
+          <h2>{inputValue.isMember ? "Login" : "Register"}</h2>
+          <div className="input-container">
+            {!inputValue.isMember && (
+              <>
+                {/* First Name */}
+                <FormLabel
+                  name="firstName"
+                  labelText="First name*"
+                  labelClass="label"
+                />
+                <FormInput
+                  type="text"
+                  name="firstName"
+                  onGetValue={getInputValueHandler}
+                  value={inputValue.firstName}
+                  required
+                  inputClass="input"
+                />
 
-      <Wrapper>
-        <main className="form-container">
-          <form onSubmit={formSubmitHandler}>
-            <div className="logo-container">
-              <Logo />
-            </div>
-            <h2>{inputValue.isMember ? "Login" : "Register"}</h2>
-            <div className="input-container">
-              {!inputValue.isMember && (
-                <>
-                  {/* First Name */}
-                  <FormLabel
-                    name="firstName"
-                    labelText="First name*"
-                    labelClass="label"
-                  />
-                  <FormInput
-                    type="text"
-                    name="firstName"
-                    onGetValue={getInputValueHandler}
-                    value={inputValue.firstName}
-                    required
-                    inputClass="input"
-                  />
+                {/* Last Name */}
+                <FormLabel
+                  name="lastName"
+                  labelText="Last name*"
+                  labelClass="label"
+                />
+                <FormInput
+                  type="text"
+                  name="lastName"
+                  onGetValue={getInputValueHandler}
+                  value={inputValue.lastName}
+                  required
+                  inputClass="input"
+                />
 
-                  {/* Last Name */}
-                  <FormLabel
-                    name="lastName"
-                    labelText="Last name*"
-                    labelClass="label"
-                  />
-                  <FormInput
-                    type="text"
-                    name="lastName"
-                    onGetValue={getInputValueHandler}
-                    value={inputValue.lastName}
-                    required
-                    inputClass="input"
-                  />
+                {/* Location */}
+                <FormLabel
+                  name="location"
+                  labelText="Your location"
+                  labelClass="label"
+                />
+                <FormInput
+                  type="text"
+                  name="location"
+                  onGetValue={getInputValueHandler}
+                  value={inputValue.userLocation}
+                  inputClass="input"
+                />
+              </>
+            )}
 
-                  {/* Location */}
-                  <FormLabel
-                    name="location"
-                    labelText="Your location"
-                    labelClass="label"
-                  />
-                  <FormInput
-                    type="text"
-                    name="location"
-                    onGetValue={getInputValueHandler}
-                    value={inputValue.location}
-                    inputClass="input"
-                  />
-                </>
-              )}
+            {/* email */}
+            <FormLabel name="email" labelText="Email*" labelClass="label" />
+            <FormInput
+              type="email"
+              name="email"
+              onGetValue={getInputValueHandler}
+              value={inputValue.email}
+              required
+              inputClass="input"
+            />
 
-              {/* email */}
-              <FormLabel name="email" labelText="Email*" labelClass="label" />
-              <FormInput
-                type="email"
-                name="email"
-                onGetValue={getInputValueHandler}
-                value={inputValue.email}
-                required
-                inputClass="input"
-              />
+            {/* password */}
+            <FormLabel
+              name="password"
+              labelText="Password*"
+              labelClass="label"
+            />
+            <FormInput
+              type="text"
+              name="password"
+              onGetValue={getInputValueHandler}
+              value={inputValue.password}
+              required
+              inputClass="input"
+            />
+          </div>
 
-              {/* password */}
-              <FormLabel
-                name="password"
-                labelText="Password*"
-                labelClass="label"
-              />
-              <FormInput
-                type="text"
-                name="password"
-                onGetValue={getInputValueHandler}
-                value={inputValue.password}
-                required
-                inputClass="input"
-              />
-            </div>
+          <button type="submit" className="submit-btn">
+            Submit
+          </button>
 
-            <button type="submit" className="submit-btn">
-              Submit
+          <p>
+            <span className="asterisk">*</span> Required fields
+          </p>
+
+          <p className="toggle-par">
+            {inputValue.isMember ? "Not a member yet?" : "Already a member?"}
+            <button
+              type="button"
+              onClick={toggleBtnHandler}
+              className="toggle-btn"
+            >
+              {inputValue.isMember ? "Register" : "Login"}
             </button>
-
-            <p>
-              <span className="asterisk">*</span> Required fields
-            </p>
-
-            <p className="toggle-par">
-              {inputValue.isMember ? "Not a member yet?" : "Already a member?"}
-              <button
-                type="button"
-                onClick={toggleBtnHandler}
-                className="toggle-btn"
-              >
-                {inputValue.isMember ? "Register" : "Login"}
-              </button>
-            </p>
-          </form>
-        </main>
-      </Wrapper>
-
+          </p>
+        </form>
+      </main>
+    </Wrapper>
   );
 };
 
