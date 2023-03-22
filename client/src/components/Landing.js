2c863d9c712180c79d00f4a054743c76ce195d14
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-03-22 15:59:36 -0700

    removed the logo from the landing page

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -2,35 +2,27 @@ import React from "react";
 import { Link } from "react-router-dom";
 import career from "../assets/images/career.svg";
 import Wrapper from "../assets/wrappers/Landing";
-import Logo from "./Logo";
 
 const Landing = () => {
   return (
-    <>
-      <div >
-        <Logo />
-      </div>
+    <Wrapper>
+      <div className="container">
+        <img src={career} alt="career" />
 
-      <Wrapper>
-        <div className="container">
-          <img src={career} alt="career" />
-
-          <div className="info-container">
-            <h1>
-              Job <span>Tracking</span> App
-            </h1>
-            <p>
-              Track job searching progress, specifying the job opportunity
-              details and always get accurate data about the status of your
-              appliations
-            </p>
-            <span className="btn-cotainer">
-              <Link to="/register">Login/Register</Link>
-            </span>
-          </div>
+        <div className="info-container">
+          <h1>
+            Job <span>Tracking</span> App
+          </h1>
+          <p>
+            Track job searching progress, specifying the job opportunity details
+            and always get accurate data about the status of your appliations
+          </p>
+          <span className="btn-cotainer">
+            <Link to="/register">Login/Register</Link>
+          </span>
         </div>
-      </Wrapper>
-    </>
+      </div>
+    </Wrapper>
   );
 };
 
