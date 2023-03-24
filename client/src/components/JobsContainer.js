ff0af94856092e69367f75690fc4257308797e85
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-03-23 18:03:33 -0700

    invoked getAllJobs context function in the JobsContainer component, removed dummy data

--- client/src/components/JobsContainer.js
+++ client/src/components/JobsContainer.js
@@ -1,88 +1,108 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Wrapper from "../assets/wrappers/JobsContainer";
 import { useAppContext } from "../context/appContext";
 import JobItem from "./sharedComponents/JobItem";
 
 const jobs = [
-  {
-    id: 1,
-    company: "Google",
-    position: "Front-end Web Developer",
-    jobLocation: "Palo Alto, CA",
-    rectuiter: "John Smith",
-    email: "john@gmail.com",
-    salary: "140 - 160",
-    jobType: "hybrid",
-    jobStatus: "interview",
-    interviewScheduled: "May 22, 12.30 PM",
-    createdAt: "March 28th, 2023",
-  },
-  {
-    id: 2,
-    company: "Amazon",
-    position: "Front-end Web Developer",
-    jobLocation: "Irvine, CA",
-    rectuiter: "Bob Meyer",
-    email: "bob@gmail.com",
-    salary: "140 - 160",
-    jobType: "full-time",
-    jobStatus: "pending",
-    interviewScheduled: "",
-    createdAt: "March 28th, 2023",
-  },
-  {
-    id: 3,
-    company: "Pinterest",
-    position: "Back-end Web Developer",
-    jobLocation: "Los Angeles, CA",
-    rectuiter: "April Smith",
-    email: "april@gmail.com",
-    salary: "140 - 160",
-    jobType: "part-time",
-    jobStatus: "declined",
-    interviewScheduled: "",
-    createdAt: "March 28th, 2023",
-  },
-  {
-    id: 4,
-    company: "Twitter",
-    position: "Front-end Web Developer",
-    jobLocation: "Palo Alto, CA",
-    rectuiter: "Glenn Mitchell",
-    email: "glenn@gmail.com",
-    salary: "140 - 160",
-    jobType: "full-time",
-    jobStatus: "pending",
-    interviewScheduled: "",
-    createdAt: "March 28th, 2023",
-  },
-  {
-    id: 5,
-    company: "Apple",
-    position: "Front-end Web Developer",
-    jobLocation: "Palo Alto, CA",
-    rectuiter: "Lisa Messer",
-    email: "lisa@gmail.com",
-    salary: "140 - 160",
-    jobType: "remote",
-    jobStatus: "interview",
-    interviewScheduled: "May 18, 11.30 AM",
-    createdAt: "March 28th, 2023",
-  },
+  // {
+  //   id: 1,
+  //   company: "Google",
+  //   position: "Front-end Web Developer",
+  //   jobLocation: "Palo Alto, CA",
+  //   rectuiter: "John Smith",
+  //   email: "john@gmail.com",
+  //   salary: "140 - 160",
+  //   jobType: "hybrid",
+  //   jobStatus: "interview",
+  //   interviewScheduled: "May 22, 12.30 PM",
+  //   createdAt: "March 28th, 2023",
+  // },
+  // {
+  //   id: 2,
+  //   company: "Amazon",
+  //   position: "Front-end Web Developer",
+  //   jobLocation: "Irvine, CA",
+  //   rectuiter: "Bob Meyer",
+  //   email: "bob@gmail.com",
+  //   salary: "140 - 160",
+  //   jobType: "full-time",
+  //   jobStatus: "pending",
+  //   interviewScheduled: "",
+  //   createdAt: "March 28th, 2023",
+  // },
+  // {
+  //   id: 3,
+  //   company: "Pinterest",
+  //   position: "Back-end Web Developer",
+  //   jobLocation: "Los Angeles, CA",
+  //   rectuiter: "April Smith",
+  //   email: "april@gmail.com",
+  //   salary: "140 - 160",
+  //   jobType: "part-time",
+  //   jobStatus: "declined",
+  //   interviewScheduled: "",
+  //   createdAt: "March 28th, 2023",
+  // },
+  // {
+  //   id: 4,
+  //   company: "Twitter",
+  //   position: "Front-end Web Developer",
+  //   jobLocation: "Palo Alto, CA",
+  //   rectuiter: "Glenn Mitchell",
+  //   email: "glenn@gmail.com",
+  //   salary: "140 - 160",
+  //   jobType: "full-time",
+  //   jobStatus: "pending",
+  //   interviewScheduled: "",
+  //   createdAt: "March 28th, 2023",
+  // },
+  // {
+  //   id: 5,
+  //   company: "Apple",
+  //   position: "Front-end Web Developer",
+  //   jobLocation: "Palo Alto, CA",
+  //   rectuiter: "Lisa Messer",
+  //   email: "lisa@gmail.com",
+  //   salary: "140 - 160",
+  //   jobType: "remote",
+  //   jobStatus: "interview",
+  //   interviewScheduled: "May 18, 11.30 AM",
+  //   createdAt: "March 28th, 2023",
+  // },
 ];
 
 const JobsContainer = () => {
-  const { showLargeSidebar } = useAppContext();
-  
+  const {
+    showLargeSidebar,
+    getAllJobs,
+    jobs,
+    isLoading,
+    numOfPages,
+    page,
+    totalJobs,
+    search,
+    searchStatus,
+    searchType,
+    sort,
+  } = useAppContext();
+
+  useEffect(() => {
+    getAllJobs();
+    // eslint-disable-next-line
+  }, [page, search, sort, searchStatus, searchType]);
+
   return (
     <Wrapper move={showLargeSidebar ? "250px" : "0px"}>
-      <h4>25 jobs found</h4>
+      <h4>
+        {totalJobs} job{jobs.length > 1 && "s"} found
+      </h4>
 
       <div className="jobs-container">
         {jobs.map((job) => {
-          return <JobItem key={job.id} {...job} />;
+          return <JobItem key={job._id} {...job} />;
         })}
       </div>
+      {/* Pages later*/}
     </Wrapper>
   );
 };
