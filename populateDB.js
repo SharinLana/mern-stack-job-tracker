01510a7a3a461cf64984d0c33abd201b223bafb3
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-03-24 15:06:48 -0700

    created a populateDB file and a function to pre-populate the database with the mockaroo data

---
+++ populateDB.js
@@ -0,0 +1,32 @@
+import { readFile } from "fs/promises";
+import mongoose from "mongoose";
+mongoose.set("strictQuery", true);
+
+import dotenv from "dotenv";
+dotenv.config();
+
+import JobModel from "./models/jobModel.js";
+
+const populate = async () => {
+  try {
+    await mongoose
+      .connect(process.env.MONGODB_URL)
+      .then(() => console.log("MONGODB CONNECTED!"))
+      .catch((err) => console.log(err));
+
+    await JobModel.deleteMany();
+
+    const jsonJobs = JSON.parse(
+      await readFile(new URL("./jobs-data.json", import.meta.url))
+    );
+    await JobModel.create(jsonJobs);
+    console.log("Success!!! 👍🏻");
+    process.exit(0);
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+};
+
+populate();
+// Then run "node populateDB.js" to fill the DB with jobs
