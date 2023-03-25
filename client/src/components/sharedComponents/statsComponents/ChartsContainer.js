6735751fb6f4468b76d5723b1c21a0a54eec6e4f
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-03-25 16:53:33 -0700

    displayed the bar chart and area chart conditionally

--- client/src/components/sharedComponents/statsComponents/ChartsContainer.js
+++ client/src/components/sharedComponents/statsComponents/ChartsContainer.js
@@ -1,20 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Wrapper from "../../../assets/wrappers/ChartsContainer";
 import BarChartComponent from "./BarChart";
 import AreaChartComponent from "./AreaChart";
 import { useAppContext } from "../../../context/appContext";
+import { set } from "mongoose";
 
 const ChartsContainer = () => {
   const { showLargeSidebar, monthlyApplications: data } = useAppContext();
+  const [barBtn, setBarBtn] = useState(true);
 
   return (
     <Wrapper move={showLargeSidebar ? "250px" : "0px"}>
       <h4>Monthly Applications</h4>
-      <button type="button" className="chart-btn">
-        Area Chart / Bar Chart
+      <button
+        type="button"
+        className="chart-btn"
+        onClick={() => setBarBtn(!barBtn)}
+      >
+        {barBtn ? "View Area Chart" : "View Bar Chart"}
       </button>
-      <BarChartComponent data={data} />
-      <AreaChartComponent data={data} />
+      {barBtn && <BarChartComponent data={data} />}
+      {!barBtn && <AreaChartComponent data={data} />}
     </Wrapper>
   );
 };
