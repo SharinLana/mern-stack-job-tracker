2564422fd3b2fd763f26d81eb6dbb45f284fd229
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-03-25 15:35:03 -0700

    invoked the getStats context function in the Stats component to receive and view the correct data format

--- client/src/components/dashboard/Stats.js
+++ client/src/components/dashboard/Stats.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import StatsContainer from "../sharedComponents/statsComponents/StatsContainer";
 import ChartsContainer from "../sharedComponents/statsComponents/ChartsContainer";
-
+import { useAppContext } from "../../context/appContext";
 
 const Stats = () => {
+  const { getStats } = useAppContext();
+
+  useEffect(() => {
+    getStats();
+  }, []);
+
   return (
     <>
       <StatsContainer />
