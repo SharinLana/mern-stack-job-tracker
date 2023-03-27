f0e3eb52a5bea17df433442f1a855243ebb003f0
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-03-27 16:27:59 -0700

    send cookies as a response in the login controller and checked the result in Postman

--- controllers/auth-controllers.js
+++ controllers/auth-controllers.js
@@ -53,6 +53,14 @@ const login = async (req, res, next) => {
   const token = user.createJWT();
   user.password = undefined;
 
+  const thirtyDays = 1000 * 60 * 60 * 24 * 30;
+
+  res.cookie("token", token, {
+    httpOnly: true,
+    expires: new Date(Date.now() + thirtyDays),
+    secure: process.env.NODE_ENV === "production",
+  }); // then test the login function in Postman and look for Cookies
+
   res.status(StatusCodes.OK).json({
     status: "success",
     token,
