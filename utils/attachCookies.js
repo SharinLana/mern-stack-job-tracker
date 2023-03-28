0e81de0f0c217c678b4ceee5b21cef12cba831ed
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-03-27 20:36:19 -0700

    testing the cookie expiration

--- utils/attachCookies.js
+++ utils/attachCookies.js
@@ -3,7 +3,7 @@ const attachCookie = ({ res, token }) => {
 
   res.cookie("token", token, {
     httpOnly: true,
-    expires: new Date(Date.now() + thirtyDays),
+    expires: new Date(Date.now() + 5000),
     secure: process.env.NODE_ENV === "production",
   }); // then test the login function in Postman and look for Cookies
 };
