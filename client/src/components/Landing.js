4cd5cee8518eb011ea4f4459ef0d9dfc9db3766b
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-03-28 17:55:02 -0700

    navigated the authorized user back to the protected route from the Landing page

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,28 +1,35 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 import career from "../assets/images/career.svg";
 import Wrapper from "../assets/wrappers/Landing";
+import { useAppContext } from "../context/appContext";
 
 const Landing = () => {
-  return (
-    <Wrapper>
-      <div className="container">
-        <img src={career} alt="career" />
+  const { user } = useAppContext();
 
-        <div className="info-container">
-          <h1>
-            Job <span>Tracking</span> App
-          </h1>
-          <p>
-            Track job searching progress, specifying the job opportunity details
-            and always get accurate data about the status of your appliations
-          </p>
-          <span className="btn-cotainer">
-            <Link to="/register">Login/Register</Link>
-          </span>
+  return (
+    <>
+      {user && <Navigate to="/" />}
+      <Wrapper>
+        <div className="container">
+          <img src={career} alt="career" />
+
+          <div className="info-container">
+            <h1>
+              Job <span>Tracking</span> App
+            </h1>
+            <p>
+              Track job searching progress, specifying the job opportunity
+              details and always get accurate data about the status of your
+              appliations
+            </p>
+            <span className="btn-cotainer">
+              <Link to="/register">Login/Register</Link>
+            </span>
+          </div>
         </div>
-      </div>
-    </Wrapper>
+      </Wrapper>
+    </>
   );
 };
 
